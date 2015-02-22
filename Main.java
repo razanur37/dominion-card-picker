bbc90b1a4733c25e75e5e56601a424d85176437d
Casey English <kcpisme@gmail.com>
2015-02-21 17:35:28 -0800

    Added logic to handle Young Witch being selected (She requires an extra 2 or 3 cost pile to be added to the supply).

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Dominion dominion = new Dominion("BASE, INTRIGUE");
+        Dominion dominion = new Dominion("CORNUCOPIA");
 
         dominion.setup();
 
